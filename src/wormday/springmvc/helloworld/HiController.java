c2b3d6e6f01a96498bb75907bb12b67970804489
oooool <liuly006@163.com>
2020-08-20 17:47:46 +0800

    test

---
+++ src/wormday/springmvc/helloworld/HiController.java
@@ -0,0 +1,14 @@
+package wormday.springmvc.helloworld;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/hi")
+public class HiController {
+
+    @RequestMapping("/say")
+    public String say() {
+        return "/WEB-INF/jsp/say.jsp";
+    }
+}
